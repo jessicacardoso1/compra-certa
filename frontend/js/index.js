659bb5e9bcf9da69cb5daf8de58feb939441338d
Filipe Silva <filipe25322@gmail.com>
2021-04-19 21:36:20 -0300

    alterações diversas relacionadas aos componenetes: nova navbar, tela de index, padronização dos botões e campos que são utilizados

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,23 +1,33 @@
-function btnMaisVendidos(id, tipoOperacao){
+function btnCounter(id, tipoOperacao){
     var campo = document.getElementById(id)
-
-    if(campo.value == "")
-        campo.value = 0
+    
+    if(campo.value < 1)
+        setBtnMaisVendidos(id, 1)
 
     if(tipoOperacao == 'sum'){
         setBtnMaisVendidos(id, parseInt(campo.value) + 1)
     }
 
     else if(tipoOperacao == 'sub'){
-        if(campo.value == 1 || campo.value == 0)
-        setBtnMaisVendidos(id, "")
+        if(campo.value == 1){
+            return
+        }
         
         campo.value = parseInt(campo.value) - 1;
     }
-    
+
 }//FIM função
 
 function setBtnMaisVendidos(id, valor){
     document.getElementById(id).value = valor;
 
 }//FIM função
+
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});
